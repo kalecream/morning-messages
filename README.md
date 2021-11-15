158152650ed11d7d37b2cbaaa7bd6747a5606179
Sabrina Medwinter <sabrinamedwinter@gmail.com>
2021-11-15 07:28:20 -0500

    [new] weather report

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 
 A simple python script to send cute messages to my boyfriend.
 
-
 ## Installation
 
 1. `git clone https://github.com/kalecream/morning-messages.git`
 to get this repository
 
-2. ` pip3 install -r requirements.txt` 
+2. `pip3 install -r requirements.txt`
 to install packages
 
-3. Change the phone number, city and country in the environment file. 
+3. Change the phone number, city and country in the environment file.
+
 ```txt
 NUMBER=+18881234567
 CITY=KINGSTON
@@ -23,7 +23,14 @@ COUNTRY=JAMAICA
 ```txt
 ⋆༶❀.⋆｡⋆༶˙⊹ .⋆. ｡⋆༶⋆˙⊹❀⋆｡⋆༶⋆˙⊹
 
-Good morning, love. I just wanted you to know how much I care for you. Have an amazing day.
+Couldn’t wait to text you good morning! Have a lovely day, babe. Today is Monday, November 11, 2021 and it's another day full of potential to give life some meaning. Night falls at 05:30:55 PM which gives you plenty of time!
+
+♡ Weather Report ♡
+
+The temperature today is 25.57°C, but it wil feel like 26.27°C. The main thing for today in weather is Clouds and they say there will be few clouds.
+
+Love Always,
+Sabrina
 
 ଘ(੭ˊ꒳​ˋ)੭✧
 ```
@@ -38,7 +45,7 @@ to install a virtual environment.
 `vitualenv messages`
 to enter the virtual environment
 
-` pip3 install -r requirements.txt` 
+`pip3 install -r requirements.txt`
 to install all packages
 
 ---
--- morning_message.py
+++ morning_message.py
@@ -1,10 +1,33 @@
-import pywhatkit, random
-from dotenv import dotenv_values
+import pywhatkit, random, requests
+from geopy.geocoders import Nominatim
+from dotenv import dotenv_values, main
+from datetime import datetime
 import resources.kaomoji as kaomoji, resources.messages as messages
 
 secret = dotenv_values(".env")
 
-msg = random.choice(kaomoji.header) + "\n\n" + random.choice(messages.content) + "\n\n" + random.choice(kaomoji.happy_kaomoji) + "\n\n"
+geolocator = Nominatim(user_agent="morning_message")
+location = geolocator.geocode(secret["CITY_NAME"] + "," + secret["COUNTRY_NAME"])
+
+try:
+    weather = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat="+str(location.latitude)+"&lon="+str(location.longitude)+"&exclude=minutely,hourly,daily&units=metric&appid="+secret["OPEN_WEATHER_API"])
+    weather.raise_for_status()
+    weather_data = weather.json()
+    sunset =  datetime.utcfromtimestamp(weather_data["current"]["sunset"]+weather_data["timezone_offset"]).strftime("%I:%M:%S %p")
+    today = datetime.utcfromtimestamp(weather_data["current"]["sunset"]+weather_data["timezone_offset"]).strftime("%A, %B %m, %Y ")
+    temperature = weather_data["current"]["temp"]
+    feels_like = weather_data["current"]["feels_like"]
+    main_weather = weather_data["current"]["weather"][0]["main"]
+    weather_report = weather_data["current"]["weather"][0]["description"]
+except weather.exceptions.HTTPError as error:
+    print(error)
+    print("Weather data not found. \nDid you add an API Key to .env?")
+    today, sunset, temperature, feels_like, main_weather, weather_report =  None
+
+if today is None:
+    msg = random.choice(kaomoji.header) + "\n\n" + random.choice(messages.content) + "\n\n" + random.choice(kaomoji.happy_kaomoji) + "\n\n"
+else:
+    msg = random.choice(kaomoji.header) + "\n\n" + random.choice(messages.content) + " Today is " + today + "and it's another day full of potential to give life some meaning. Night falls at " + sunset + " which gives you plenty of time!"+ "\n\n" + "♡ Weather Report ♡\n\n" + "The temperature today is " + str(temperature) + "°C, but it wil feel like " + str(feels_like) + "°C. The main thing for today in weather is " +  main_weather + " and they say there will be " + weather_report + ".\n\n" + "Love Always,\n"+ secret["NAME"] +  "\n\n" + random.choice(kaomoji.happy_kaomoji or kaomoji.kiss_kaomoji or kaomoji.love_kaomoji)
 print(msg)
 
 pywhatkit.sendwhatmsg_instantly(secret["NUMBER"],msg,20,False)
