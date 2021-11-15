9ffe10905f8957b0091e7376bf7da8f1598ad5fd
Sabrina Medwinter <sabrinamedwinter@gmail.com>
2021-11-15 05:23:56 -0500

    [fix] env

--- morning_message.py
+++ morning_message.py
@@ -1,10 +1,10 @@
 import pywhatkit, random
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 import resources.kaomoji as kaomoji, resources.messages as messages
 
-load_dotenv(NUMBER)
+secret = dotenv_values(".env")
 
 msg = random.choice(kaomoji.header) + "\n\n" + random.choice(messages.content) + "\n\n" + random.choice(kaomoji.happy_kaomoji) + "\n\n"
 print(msg)
 
-pywhatkit.sendwhatmsg_instantly(NUMBER,msg,20,False)
+pywhatkit.sendwhatmsg_instantly(secret["NUMBER"],msg,20,False)
